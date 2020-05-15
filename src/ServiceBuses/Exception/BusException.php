46dde60fd35ee466dad0eb3502f452f797dda348
Anton Karpov <awd.com.ua@gmail.com>
2020-05-16 01:21:55 +0300

    Changed exception's level to Runtime ones

--- src/ServiceBuses/Exception/BusException.php
+++ src/ServiceBuses/Exception/BusException.php
@@ -4,4 +4,4 @@ declare(strict_types=1); // strict mode
 
 namespace AwdStudio\ServiceBuses\Exception;
 
-abstract class BusException extends \Exception {}
+abstract class BusException extends \RuntimeException {}
